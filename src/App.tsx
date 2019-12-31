0b4109010a607b837ef29ba88d63a5e4e8067ce9
Ilin Kirill <kamushek.stone@gmail.com>
2019-12-31 18:30:12 +0300

    Start of the routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Schedule from './Components/Schedule';
+import CSS from 'csstype'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useParams
+} from 'react-router-dom'
 import './App.css';
 
-type option = { value: string, label: string, color: string, isFixed?: boolean, isDisabled?: undefined };
-
-const App: React.FC = () => {
-
-  let [vars, setVars] = useState([{ name: 'lol', value: 'lalka' }, { name: 'lol2', value: 'lalkachen' }]);
+const App = () => {
   return (
-    <div className="App">
-      <Schedule groups={["6374"]} />
-    </div>
+    <div className="App" style={topMenuStyle}>
+      <Router>
+        <div className="topMenu">
+          <Link to="/">Домашняя</Link>
+          <Link to="/schedule">Расписание</Link>
+          <Link to="/tasks">Задания</Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <p>Тут типа текст стандартного пути</p>
+          </Route>
+          <Route path="/schedule">
+            <Schedule groups={["6374"]} />
+          </Route>
+          <Route path="/tasks/:id">
+            <Tasks />
+          </Route>
+          <Route path="/tasks">
+            <p>Тут будет полный список заданий</p>
+          </Route>
+        </Switch>
+      </Router >
+    </div >
+  );
+}
+
+const Tasks = () => {
+  let { id } = useParams();
+  return (
+    <>
+      <p>Тут будет задание с id = {id}</p>
+    </>
   );
 }
 
 export default App;
+
+const topMenuStyle: CSS.Properties = {
+  display: 'block',
+};
